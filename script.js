023b69ee7b39e0bbd74f697713f4615eb30c8e2b
arunraj3 <arunrajgovindan2002@gmail.com>
2024-03-08 12:34:48 +0530

    sample 1 complete

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let submitButton = document.getElementById("get-fact-button")
+let inputElement = document.getElementById("number-input")
+async function getNumber(){
+    
+    if(inputElement.value==''){inputElement.value = 0};
+    const response = await fetch(`http://numbersapi.com/${inputElement.value}/math`);
+    const fact = await response.text();
+    let factDisplay = document.getElementById("fact-display");
+    factDisplay.innerHTML = fact;
+}
+
+
+submitButton.addEventListener("click",getNumber)
+
+inputElement.addEventListener("keypress",(event)=>{if(event.key==="Enter"){
+    event.preventDefault();
+    submitButton.click();
+}})
